15f8eaa480088bb90d20705a4fe2c78330a0b9be
Stars Hu <hxldxx99@sina.com>
2016-08-24 14:02:25 +0800

    Change to websocket

--- chatdemo.py
+++ chatdemo.py
@@ -13,117 +13,92 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+"""Simplified chat demo for websockets.
+
+Authentication, error handling, etc are left as an exercise for the reader :)
+"""
 
 import logging
 import tornado.escape
 import tornado.ioloop
+import tornado.options
 import tornado.web
+import tornado.websocket
 import os.path
 import uuid
 
-from tornado.concurrent import Future
-from tornado import gen
-from tornado.options import define, options, parse_command_line
+from tornado.options import define, options
 
 define("port", default=8888, help="run on the given port", type=int)
-define("debug", default=False, help="run in debug mode")
 
 
-class MessageBuffer(object):
+class Application(tornado.web.Application):
     def __init__(self):
-        self.waiters = set()
-        self.cache = []
-        self.cache_size = 200
-
-    def wait_for_messages(self, cursor=None):
-        # Construct a Future to return to our caller.  This allows
-        # wait_for_messages to be yielded from a coroutine even though
-        # it is not a coroutine itself.  We will set the result of the
-        # Future when results are available.
-        result_future = Future()
-        if cursor:
-            new_count = 0
-            for msg in reversed(self.cache):
-                if msg["id"] == cursor:
-                    break
-                new_count += 1
-            if new_count:
-                result_future.set_result(self.cache[-new_count:])
-                return result_future
-        self.waiters.add(result_future)
-        return result_future
-
-    def cancel_wait(self, future):
-        self.waiters.remove(future)
-        # Set an empty result to unblock any coroutines waiting.
-        future.set_result([])
-
-    def new_messages(self, messages):
-        logging.info("Sending new message to %r listeners", len(self.waiters))
-        for future in self.waiters:
-            future.set_result(messages)
-        self.waiters = set()
-        self.cache.extend(messages)
-        if len(self.cache) > self.cache_size:
-            self.cache = self.cache[-self.cache_size:]
-
-
-# Making this a non-singleton is left as an exercise for the reader.
-global_message_buffer = MessageBuffer()
+        handlers = [
+            (r"/", MainHandler),
+            (r"/chatsocket", ChatSocketHandler),
+        ]
+        settings = dict(
+            cookie_secret="123456",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            xsrf_cookies=True,
+        )
+        super(Application, self).__init__(handlers, **settings)
 
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("index.html", messages=global_message_buffer.cache)
+        self.render("index.html", messages=ChatSocketHandler.cache)
 
 
-class MessageNewHandler(tornado.web.RequestHandler):
-    def post(self):
-        message = {
+class ChatSocketHandler(tornado.websocket.WebSocketHandler):
+    waiters = set()
+    cache = []
+    cache_size = 200
+
+    def get_compression_options(self):
+        # Non-None enables compression with default options.
+        return {}
+
+    def open(self):
+        ChatSocketHandler.waiters.add(self)
+
+    def on_close(self):
+        ChatSocketHandler.waiters.remove(self)
+
+    @classmethod
+    def update_cache(cls, chat):
+        cls.cache.append(chat)
+        if len(cls.cache) > cls.cache_size:
+            cls.cache = cls.cache[-cls.cache_size:]
+
+    @classmethod
+    def send_updates(cls, chat):
+        logging.info("sending message to %d waiters", len(cls.waiters))
+        for waiter in cls.waiters:
+            try:
+                waiter.write_message(chat)
+            except:
+                logging.error("Error sending message", exc_info=True)
+
+    def on_message(self, message):
+        logging.info("got message %r", message)
+        parsed = tornado.escape.json_decode(message)
+        chat = {
             "id": str(uuid.uuid4()),
-            "body": self.get_argument("body"),
+            "body": parsed["body"],
         }
-        # to_basestring is necessary for Python 3's json encoder,
-        # which doesn't accept byte strings.
-        message["html"] = tornado.escape.to_basestring(
-            self.render_string("message.html", message=message))
-        if self.get_argument("next", None):
-            self.redirect(self.get_argument("next"))
-        else:
-            self.write(message)
-        global_message_buffer.new_messages([message])
+        chat["html"] = tornado.escape.to_basestring(
+            self.render_string("message.html", message=chat))
 
-
-class MessageUpdatesHandler(tornado.web.RequestHandler):
-    @gen.coroutine
-    def post(self):
-        cursor = self.get_argument("cursor", None)
-        # Save the future returned by wait_for_messages so we can cancel
-        # it in wait_for_messages
-        self.future = global_message_buffer.wait_for_messages(cursor=cursor)
-        messages = yield self.future
-        if self.request.connection.stream.closed():
-            return
-        self.write(dict(messages=messages))
-
-    def on_connection_close(self):
-        global_message_buffer.cancel_wait(self.future)
+        ChatSocketHandler.update_cache(chat)
+        ChatSocketHandler.send_updates(chat)
 
 
 def main():
-    parse_command_line()
-    app = tornado.web.Application(
-        [
-            (r"/", MainHandler),
-            (r"/a/message/new", MessageNewHandler),
-            (r"/a/message/updates", MessageUpdatesHandler),
-            ],
-        cookie_secret="123456",
-        template_path=os.path.join(os.path.dirname(__file__), "templates"),
-        static_path=os.path.join(os.path.dirname(__file__), "static"),
-        # xsrf_cookies=True,
-        debug=options.debug,
-        )
+    tornado.options.parse_command_line()
+    app = Application()
     app.listen(options.port)
     tornado.ioloop.IOLoop.current().start()
 
--- static/chat.js
+++ static/chat.js
@@ -25,41 +25,17 @@ $(document).ready(function() {
             newMessage($(this));
             return false;
         }
-        return true;
     });
     $("#message").select();
-    updater.poll();
+    updater.start();
 });
 
 function newMessage(form) {
     var message = form.formToDict();
-    var disabled = form.find("input[type=submit]");
-    disabled.disable();
-    $.postJSON("/a/message/new", message, function(response) {
-        updater.showMessage(response);
-        if (message.id) {
-            form.parent().remove();
-        } else {
-            form.find("input[type=text]").val("").select();
-            disabled.enable();
-        }
-    });
+    form['method'] = "ADD";
+    updater.socket.send(JSON.stringify(message));
+    form.find("input[type=text]").val("").select();
 }
-
-function getCookie(name) {
-    var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
-    return r ? r[1] : undefined;
-}
-
-jQuery.postJSON = function(url, args, callback) {
-    args._xsrf = getCookie("_xsrf");
-    $.ajax({url: url, data: $.param(args), dataType: "text", type: "POST",
-            success: function(response) {
-        if (callback) callback(eval("(" + response + ")"));
-    }, error: function(response) {
-        console.log("ERROR:", response);
-    }});
-};
 
 jQuery.fn.formToDict = function() {
     var fields = this.serializeArray();
@@ -71,57 +47,14 @@ jQuery.fn.formToDict = function() {
     return json;
 };
 
-jQuery.fn.disable = function() {
-    this.enable(false);
-    return this;
-};
-
-jQuery.fn.enable = function(opt_enable) {
-    if (arguments.length && !opt_enable) {
-        this.attr("disabled", "disabled");
-    } else {
-        this.removeAttr("disabled");
-    }
-    return this;
-};
-
 var updater = {
-    errorSleepTime: 500,
-    cursor: null,
+    socket: null,
 
-    poll: function() {
-        var args = {"_xsrf": getCookie("_xsrf")};
-        if (updater.cursor) args.cursor = updater.cursor;
-        $.ajax({url: "/a/message/updates", type: "POST", dataType: "text",
-                data: $.param(args), success: updater.onSuccess,
-                error: updater.onError});
-    },
-
-    onSuccess: function(response) {
-        try {
-            updater.newMessages(eval("(" + response + ")"));
-        } catch (e) {
-            updater.onError();
-            return;
-        }
-        updater.errorSleepTime = 500;
-        window.setTimeout(updater.poll, 0);
-    },
-
-    onError: function(response) {
-        updater.errorSleepTime *= 2;
-        console.log("Poll error; sleeping for", updater.errorSleepTime, "ms");
-        window.setTimeout(updater.poll, updater.errorSleepTime);
-    },
-
-    newMessages: function(response) {
-        if (!response.messages) return;
-        updater.cursor = response.cursor;
-        var messages = response.messages;
-        updater.cursor = messages[messages.length - 1].id;
-        console.log(messages.length, "new messages, cursor:", updater.cursor);
-        for (var i = 0; i < messages.length; i++) {
-            updater.showMessage(messages[i]);
+    start: function() {
+        var url = "ws://" + location.host + "/chatsocket";
+        updater.socket = new WebSocket(url);
+        updater.socket.onmessage = function(event) {
+            updater.showMessage(JSON.parse(event.data));
         }
     },
 
